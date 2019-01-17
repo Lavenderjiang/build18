80941e6fd8423e8d6895dadff94ba16096766e4f
Lavenderjiang <lavenderchiang@gmail.com>
2019-01-16 19:16:14 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,54 @@
+console.log("Hello!")
+
+var Particle = require('particle-api-js');
+var particle = new Particle();
+var token;
+
+// order of function execution
+init();
+//getDevice();
+
+function init(){
+particle.login({username: 'mohideen.syed@rocketmail.com', password: 'Build18'}).then(
+  function(data) {
+    token = data.body.access_token;
+    console.log('API call completed on promise resolve: ', data.body.access_token);
+        token = data.body.access_token;
+
+  // get device
+    var devicesPr = particle.listDevices({ auth: token });
+
+    devicesPr.then(
+      function(devices){
+        console.log('Devices: ', devices);
+      },
+      function(err) {
+        console.log('List devices call failed: ', err);
+      }
+    );
+
+    // invoke function
+    var fnPr = particle.callFunction({ deviceId: '37001d000751373238323937', 
+    name: 'ctrlled', argument: ',HIGH', auth: token });
+
+    fnPr.then(
+    function(data) {
+      console.log('Function called succesfully:', data);
+    }, function(err) {
+      console.log('An error occurred:', err);
+    });
+      },
+      function (err) {
+        console.log('Could not log in.', err);
+      }
+    );
+}
+
+function getDevice(){
+  console.log('current token is', token);
+
+}
+
+
+
+
